b61d36d2b9c04a8764195e67e9d142535fccd106
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2021-11-26 18:43:23 +0530

    Delete 557. Reverse Words in a String III.py

--- 557. Reverse Words in a String III.py
+++
@@ -1,8 +0,0 @@
-#Simple Approach
-
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        a = s.split(" ")
-        for j in range(len(a)):
-            a[j] =a[j][::-1]
-        return " ".join(a)
